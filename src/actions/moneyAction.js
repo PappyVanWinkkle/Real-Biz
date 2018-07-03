309e5264804a71b942e5a783249b33f7093a715e
chabz <chabz@chabzs-MacBook.local>
2018-07-03 01:23:42 +0100

    Made changes to the application

--- src/actions/moneyAction.js
+++ src/actions/moneyAction.js
@@ -7,11 +7,11 @@
 import axios from 'axios';
 import { GET_ALL_INFORMATION } from './types';
 
-//const app_id = `ee94393f`;
-//const api_key = `75600454dfb1a011dda06b02ecfdecc1`;
+const api_key = `58e269f14517bf680d74f7cbf85dc405`;
 
 export default function getMarketInformation() {
-	const baseURL = `https://marketdata.websol.barchart.com/getHistory.json?apikey=58e269f14517bf680d74f7cbf85dc405&symbol=IBM&type=daily&startDate=20160608000000`;
+	//const baseURL = `https://marketdata.websol.barchart.com/getHistory.json?apikey=${api_key}&symbol=IBM&type=daily&startDate=20160608000000`;
+	const baseURL = `https://api.iextrading.com/1.0/stock/aapl/chart/6m`;
 	return function(dispatch) {
 		axios
 			.get(`${baseURL}`)
--- src/components/marketInfo.js
+++ src/components/marketInfo.js
@@ -6,36 +6,60 @@
 
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { ListGroup, ListGroupItem, Badge } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 import { connect } from 'react-redux';
 import getMarketInformation from '../actions/moneyAction';
-import Footer from './Footer';
 
 class MarketInfo extends Component {
 	componentDidMount() {
 		this.props.getMarketInformation();
 	}
 	render() {
-		console.log(this.props.money.money.results);
+		console.log(this.props.money);
+
 		return (
-			<div style={{ background: '#fff' }}>
-				<div className="intro-text">
-					<h2 style={{ textAlign: 'center', fontFamily: "'Permanent Marker', cursive", marginTop: '3em' }}>
-						Ready Steady Cook!
-					</h2>
-				</div>
-				<Footer />
-			</div>
+			<Container>
+				<Row>
+					<Col xs="6" sm="6">
+						{this.props.money.map((key) => {
+							return (
+								<ListGroup>
+									<ListGroupItem className="justify-content-between">
+										Date <Badge pill>{key.date}</Badge>
+									</ListGroupItem>
+									<ListGroupItem className="justify-content-between">
+										Open <Badge pill>{key.open}</Badge>
+									</ListGroupItem>
+									<ListGroupItem className="justify-content-between">
+										High <Badge pill>{key.high}</Badge>
+									</ListGroupItem>
+									<ListGroupItem className="justify-content-between">
+										Low <Badge pill>{key.low}</Badge>
+									</ListGroupItem>
+									<ListGroupItem className="justify-content-between">
+										Close <Badge pill>{key.close}</Badge>
+									</ListGroupItem>
+								</ListGroup>
+							);
+						})}
+					</Col>
+					<Col xs="6" sm="6">
+						News Here
+					</Col>
+				</Row>
+			</Container>
 		);
 	}
 }
 
 MarketInfo.propTypes = {
 	getMarketInformation: PropTypes.func.isRequired,
-	money: PropTypes.object.isRequired
+	money: PropTypes.array.isRequired
 };
 
 const mapStateToProps = (state) => ({
-	money: state.money
+	money: state.money.money
 });
 
 export default connect(mapStateToProps, { getMarketInformation })(MarketInfo);
